dc66c28cceedf871ca3ef9eac16fc50a8c867e5f
Ryan Kline <ryankline4780@gmail.com>
2022-04-09 12:15:59 -0400

    Updated Pi with transactions frame + bug fixes

--- ATM_GUI_Ryan.py
+++ ATM_GUI_Ryan.py
@@ -8,10 +8,14 @@ import RPi.GPIO as GPIO
 import pn532.pn532 as nfc
 from pn532 import *
 import ATM
+import sqlite3
 
 import sys
 import time
 
+
+ATM.createTable()
+ATM.createTransactionTable()
 
 pn532 = PN532_SPI(cs=4, reset=20, debug=False)
 ic, ver, rev, support = pn532.get_firmware_version()
@@ -169,7 +173,7 @@ class Dashboard(tk.Frame):
 
         transfer_button = tk.Button(
             self, text="Recent Transactions", padx=45, pady=18, fg="white", bg='#343332', font=("Arial Bold", 10),
-            command=lambda: controller.show_frame("TransactionsFrame"))
+            command=lambda: [controller.show_frame("TransactionsFrame"), TransactionsFrame.populateTransactions()])
         transfer_button.place(x=475, y=250)
 
         PIN_button = tk.Button(
@@ -178,7 +182,7 @@ class Dashboard(tk.Frame):
         PIN_button.place(x=475, y=350)
 
         logoutButton = tk.Button(self, text="Logout", padx=10, pady=10, font=("Arial Bold", 10),
-                                 command=lambda: [controller.show_frame("StartPage"), ATM.logoutAll()])
+                                 command=lambda: [controller.show_frame("StartPage"), ATM.logout()])
         logoutButton.place(x=30, y=10)
 
 
@@ -198,7 +202,7 @@ class LoginPage(tk.Frame):
         global Welcome_name_label
         Welcome_name_label = Label(controller.frames["WelcomePage"], text="Hello ",
                             bg='#FEFEFE', font=("Arial Bold", 25))
-        Welcome_name_label.place(x=315, y=20)
+        Welcome_name_label.place(x=285, y=20)
 
         global Secondary_label
         Secondary_label = Label(self, text="Waiting",
@@ -227,7 +231,7 @@ class LoginPage(tk.Frame):
 
         login(uidStr)
         print(str(ATM.currUser.name))
-        Welcome_name_label.config(text= str(ATM.currUser.name))
+        Welcome_name_label.config(text= "Hello " + str(ATM.currUser.name))
         root.show_frame("WelcomePage")
 
 class moneyMoves(tk.Frame):
@@ -311,10 +315,12 @@ class DepositFrame(tk.Frame):
              Handles the deposit/withdraw functionality
             """
             money = amount_entry.get()
+            money_str = "+" + "${:,.2f}".format(float(money))
             try:
                 ATM.currUser.deposit(float(money))
                 update_p2_label(self)
                 ATM.updateBalance()
+                ATM.newTransaction(ATM.currUser.cardNum, money_str)
                 controller.show_frame("Dashboard")
             except ValueError:
                 messagebox.showerror("Error",
@@ -368,10 +374,12 @@ class WithdrawFrame(tk.Frame):
              Handles the deposit/withdraw functionality
             """
             money = amount_entry.get()
+            money_str = "-" + "${:,.2f}".format(float(money))
             try:
                 ATM.currUser.withdraw(float(money))
                 update_p2_label(self)
                 ATM.updateBalance()
+                ATM.newTransaction(ATM.currUser.cardNum, money_str)
                 controller.show_frame("Dashboard")
             except ValueError:
                 messagebox.showerror("Error",
@@ -434,11 +442,15 @@ class TransferFrame(tk.Frame):
             """
             recipient = accountNum_entry.get()
             money = amount_entry.get()
+            usr_money_str = "-" + "${:,.2f}".format(float(money))
+            recipient_money_str = "+" + "${:,.2f}".format(float(money))
             try:
                 if(str(recipient) != str(ATM.currUser.cardNum)):
                     ATM.currUser.transfer(money, recipient)
                     update_p2_label(self)
                     ATM.updateBalance()
+                    ATM.newTransaction(ATM.currUser.cardNum, usr_money_str)
+                    ATM.newTransaction(recipient, recipient_money_str)
                     controller.show_frame("Dashboard")
                 else:
                     messagebox.showerror("Error",
@@ -473,52 +485,56 @@ class TransactionsFrame(tk.Frame):
             self, text="Recent Transactions", padx=10, pady=10, bg="white", font=("Arial Bold", 25))
         header_label.place(x=245, y=10)
 
-        heading1_label = Label(
-            self, text="Amount:", padx=10, pady=10, bg="white", font=("Arial Bold", 10)
-        )
-        heading1_label.place(x=250, y=100)
-
-        heading2_label = Label(
-            self, text="Initiated By:", padx=10, pady=10, bg="white", font=("Arial Bold", 10)
-        )
-        heading2_label.place(x=450, y=100)
-
         # Transaction Labels
+        global t1_amount_label
         t1_amount_label = Label(
-            self, text="--", padx=10, pady=10, bg="white", font=("Arial Bold", 10)
+            self, text="1: --", padx=10, pady=10, bg="white", font=("Arial Bold", 15)
         )
-        t1_amount_label.place(x=250, y=150)
+        t1_amount_label.place(x=250, y=120)
 
-        t1_User_label = Label(
-            self, text="--", padx=10, pady=10, bg="white", font=("Arial Bold", 10)
-        )
-        t1_User_label.place(x=450, y=150)
-
+        global t2_amount_label
         t2_amount_label = Label(
-            self, text="--", padx=10, pady=10, bg="white", font=("Arial Bold", 10)
+            self, text="2: --", padx=10, pady=10, bg="white", font=("Arial Bold", 15)
         )
-        t2_amount_label.place(x=250, y=200)
+        t2_amount_label.place(x=250, y=190)
 
-        t2_User_label = Label(
-            self, text="--", padx=10, pady=10, bg="white", font=("Arial Bold", 10)
-        )
-        t2_User_label.place(x=450, y=200)
-
+        global t3_amount_label
         t3_amount_label = Label(
-            self, text="--", padx=10, pady=10, bg="white", font=("Arial Bold", 10)
+            self, text="3: --", padx=10, pady=10, bg="white", font=("Arial Bold", 15)
         )
-        t3_amount_label.place(x=250, y=250)
-
-        t3_User_label = Label(
-            self, text="--", padx=10, pady=10, bg="white", font=("Arial Bold", 10)
-        )
-        t3_User_label.place(x=450, y=250)
+        t3_amount_label.place(x=250, y=260)
 
         submit_button = tk.Button(
             self, text="OK", padx=60, pady=18, fg="white", bg='#343332', font=("Arial Bold", 10),
             command=lambda: controller.show_frame("Dashboard"))
         submit_button.place(x=340, y=390)
 
+    def populateTransactions():
+        db = sqlite3.connect('user_info.db')
+        c = db.cursor()
+        c.execute("SELECT t1 FROM transactions WHERE cardNum=?",
+                  (ATM.currUser.cardNum,))
+        t1 = c.fetchone()[0]
+        if(t1 != "0"):
+            t1_amount_label.config(text="1: " + t1)
+        else:
+            t1_amount_label.config(text="1: --")
+
+        c.execute("SELECT t2 FROM transactions WHERE cardNum=?",
+                  (ATM.currUser.cardNum,))
+        t2 = c.fetchone()[0]
+        if(t2 != "0"):
+            t2_amount_label.config(text="2: " + t2)
+        else:
+            t2_amount_label.config(text="2: --")
+
+        c.execute("SELECT t3 FROM transactions WHERE cardNum=?",
+                  (ATM.currUser.cardNum,))
+        t3 = c.fetchone()[0]
+        if(t3 != "0"):
+            t3_amount_label.config(text="3: " + t3)
+        else:
+            t3_amount_label.config(text="3: --")
 
 class PINChangeFrame1(tk.Frame):
 
@@ -648,7 +664,8 @@ class PINChangeFrame2(tk.Frame):
 
 
 def login(cardUID):
-    ATM.logoutAll()
+    if(ATM.currUser.name != None):
+        ATM.logout()
     expectedCardNum = ATM.get_key(cardUID)
     ATM.login(expectedCardNum)
     root.displayText.set("${:,.2f}".format(ATM.currUser.balance))
@@ -657,4 +674,5 @@ def login(cardUID):
 
 if __name__ == "__main__":
     root = Controller()
+    root.title("Lakers Credit Union")
     root.mainloop()
